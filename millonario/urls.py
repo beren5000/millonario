93e8575b769d66a603b565adbe4914e8a4230ffe
beren5000 <beren.monsalve@gmail.com>
2012-12-10 13:54:25 -0500

    urls

--- millonario/urls.py
+++ millonario/urls.py
@@ -15,12 +15,12 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^encuestas/', include('millonario.modulos.encuestas.urls')),
-    url(r'^$', direct_to_template,
+    (r'^quien_la_tiene_clara/', include('millonario.modulos.encuestas.urls')),
+    url(r'Inicio^$', direct_to_template,
         {'template': 'index.html'},
         name='home'),
 
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
 
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': MEDIA_ROOT})
