84650a574b1e4297ed47d9eba3dfc22e068ba646
beren5000 <beren.monsalve@gmail.com>
2012-12-18 22:02:35 -0500

    login full

--- millonario/settings.py
+++ millonario/settings.py
@@ -177,3 +177,10 @@ LOGGING = {
 ELEMENTOS_POR_PAGINA=10
 LOGIN_REDIRECT_URL = '/inicio/'
 LOGIN_URL = '/'
+TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
+                               "django.core.context_processors.debug",
+                               "django.core.context_processors.i18n",
+                               "django.core.context_processors.media",
+                               "django.core.context_processors.static",
+                               "django.core.context_processors.tz",
+                               "django.contrib.messages.context_processors.messages")
--- millonario/urls.py
+++ millonario/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
 from millonario.settings import MEDIA_ROOT
 
@@ -17,9 +18,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
     (r'^quien_la_tiene_clara/', include('millonario.modulos.encuestas.urls')),
-    url(r'^inicio/$', direct_to_template, {'template': 'index.html'}, name='inicio'),
+    url(r'^inicio/$', login_required(direct_to_template), {'template': 'index.html'}, name='inicio'),
 
-
+    url(r'^salir/$', 'django.contrib.auth.views.logout',
+        {'template_name': 'login.html'},
+        name='logout'),
 
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': MEDIA_ROOT})
